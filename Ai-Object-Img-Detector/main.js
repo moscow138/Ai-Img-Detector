1cf35149713bfa82bd08c01c039550661ce0b180
john ogheneochuko <ochustech56@gmail.com>
2024-03-19 21:42:33 +0100

    imported transfomer.js CDN

--- Ai-Object-Img-Detector/main.js
+++ Ai-Object-Img-Detector/main.js
@@ -1,23 +1,21 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {
+  env,
+  pipeline
+} from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.0'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-setupCounter(document.querySelector('#counter'))
+import './style.css'
+import './utils/bling'
+
+console.log({ env, pipeline })
+
+env.allowLocalModels = false
+
+// eslint-disable-next-line no-undef
+const STATUS = $('#status')
+const IMAGE = $('image-container')
+const FILEINPUT = $('file-upload')
+
+// load status text..
+STATUS.textContent = 'Loading model...'
+const DETECTOR = await pipeline('object-detection', 'Xenova/detr-resnet-50')
+STATUS.textContent = 'Ready'
