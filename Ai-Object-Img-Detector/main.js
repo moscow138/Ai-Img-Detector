d5a7c80040561a2cfd42bed7eff3ff4c8314e4b2
john ogheneochuko <ochustech56@gmail.com>
2024-03-19 22:07:20 +0100

    added new Lines of code...

--- Ai-Object-Img-Detector/main.js
+++ Ai-Object-Img-Detector/main.js
@@ -12,10 +12,13 @@ env.allowLocalModels = false
 
 // eslint-disable-next-line no-undef
 const STATUS = $('#status')
-const IMAGE = $('image-container')
-const FILEINPUT = $('file-upload')
+const IMAGE = $('#image-container')
+const FILEINPUT = $('#file-upload')
 
 // load status text..
 STATUS.textContent = 'Loading model...'
 const DETECTOR = await pipeline('object-detection', 'Xenova/detr-resnet-50')
 STATUS.textContent = 'Ready'
+FILEINPUT.on('change', (event) => {
+  console.log('File input change event', event)
+})
